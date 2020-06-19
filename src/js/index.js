becfa641920c43567ed2cba92cc1bb28f9c3aa59
Michał Sypniewski <mi.sypniewski@gmail.com>
2020-06-19 13:35:46 +0200

    work in progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,52 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
+// import { registerSW } from './pwa.js';
 // registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const addButton = document.querySelector('.clients__button--add-js');
+const removeButton = document.querySelector('.clients__button--remove-js');
+const counterHtmlElement = document.querySelector('.clients__counter--js');
+const messageHtmlElement = document.querySelector('.clients__message--js');
+let counter = 0;
+const maxClients = 30;
+let message = "";
+
+const handleChangeClientCounter = (changes) => {
+  if (changes === "add") {
+    if (counter >= maxClients) {
+      message = "Maksymalna ilość klientów!"
+    } else {
+      counter++;
+      message = "";
+    }
+  } else {
+    if (counter <= 0) {
+      message = "Brak klientów na sklepie"
+    } else {
+      counter--;
+      message = "";
+    }
+  }
+
+  showClients();
+}
+
+const showClients = () => {
+  counterHtmlElement.innerHTML = counter;
+  messageHtmlElement.innerHTML = message;
+}
+
+addButton.addEventListener("click", () => {
+  handleChangeClientCounter("add");
+})
+
+removeButton.addEventListener("click", () => {
+  handleChangeClientCounter("remove");
+})
+
+
+
+// console.log('HELLO 🚀')
